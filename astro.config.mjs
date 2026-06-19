8b9e5c7922180d256e71db93e1310bde8495ea56
_Spark <spark-aur@proton.me>
2026-06-19 16:27:11 +0300

    github pages time?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
+  site: "https://sparkhere-sys.github.io",
+  base: "/vftf/",
+
   vite: {
     plugins: [
       yaml()
